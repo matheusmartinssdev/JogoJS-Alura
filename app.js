d6a5a8eb9b12ee5d69582cc46f6b457c09e6951a
MATHEUS MIGUEL MARTINS DE OLIVEIRA <matheus.oliveira@pmc.local>
2026-04-20 16:13:13 -0300

    add new conditional for chute

--- app.js
+++ app.js
@@ -7,5 +7,9 @@ let chute = prompt("Escolha um número entre 1 e 10");
 if (chute == numeroSecreto) {
     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
 } else {
-    alert('Você errou :(')
+    if (chute > numeroSecreto){
+        alert(`O número secreto é menor que ${chute}`);
+    } else{
+        alert(`O número secreto é maior que ${chute}`);
+    }
 }

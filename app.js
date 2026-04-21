de276b6d37e0c39aa64f28d302a3e93b4db8a108
Matheus Miguel <matheusmiguel960@gmail.com>
2026-04-21 18:25:13 -0300

    add num tentativas

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 alert("Boas vindas ao jogo do número secreto");
 let numeroSecreto = 4;
-console.log(numeroSecreto)
-let chute
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
 //enquanto chute não for igual ao n.s
 while (chute != numeroSecreto) {
     chute = prompt("Escolha um número entre 1 a 10");
     // se chute for igual ao n.s
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
     } else {
         if (chute > numeroSecreto){
             alert(`O número secreto é menor que ${chute}`);
         } else{
             alert(`O número secreto é maior que ${chute}`);
-        }   
+        }
+        tentativas++;
     }
 }
 

b32f4f62fdd7fc6474e1f8b96c9a4925c1007c35
MATHEUS MIGUEL MARTINS DE OLIVEIRA <matheus.oliveira@pmc.local>
2026-04-22 16:32:49 -0300

    alert removed while loop

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ while (chute != numeroSecreto) {
     chute = prompt("Escolha um número entre 1 a 10");
     // se chute for igual ao n.s
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+        break;
     } else {
         if (chute > numeroSecreto){
             alert(`O número secreto é menor que ${chute}`);
@@ -19,4 +19,6 @@ while (chute != numeroSecreto) {
         tentativas++;
     }
 }
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+
 
